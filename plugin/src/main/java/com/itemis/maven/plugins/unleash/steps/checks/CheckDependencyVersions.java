962d0301975542c6ab615f1295173121740ea3d8
Stanley Hillner <shillner@users.noreply.github.com>
2016-06-06 10:39:33 +0200

    fixed ConcurrentModificationException

--- plugin/src/main/java/com/itemis/maven/plugins/unleash/steps/checks/CheckDependencyVersions.java
+++ plugin/src/main/java/com/itemis/maven/plugins/unleash/steps/checks/CheckDependencyVersions.java
@@ -18,6 +18,7 @@ import org.apache.maven.project.MavenProject;
 import com.google.common.base.Objects;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import com.itemis.maven.plugins.cdi.CDIMojoProcessingStep;
@@ -117,13 +118,17 @@ public class CheckDependencyVersions implements CDIMojoProcessingStep {
         ProjectToString.INCLUDE_PACKAGING);
 
     for (MavenProject p : snapshotByProject.keySet()) {
+      List<String> dependenciesToRemove = Lists.newArrayList();
       for (String dep : snapshotByProject.get(p)) {
         for (String moduleCoordinate : projectCoordinates) {
           if (Objects.equal(dep, moduleCoordinate)) {
-            snapshotByProject.remove(p, dep);
+            dependenciesToRemove.add(dep);
             break;
           }
         }
+      }
+      for (String dep : dependenciesToRemove) {
+        snapshotByProject.remove(p, dep);
       }
     }
   }
