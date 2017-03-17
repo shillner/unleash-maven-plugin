6bedf9e5147d4657de902cef6b58156b934fe36a
Stanley <Stanley@DESKTOP-L2D0HBJ>
2017-03-17 08:43:54 +0100

    #68: Plugin need property resolution for inactive profiles
    Fixed existing tests after necessary changes for implementation of
    property resolver.

--- plugin/src/test/java/com/itemis/maven/plugins/unleash/util/predicates/IsSnapshotDependencyTest.java
+++ plugin/src/test/java/com/itemis/maven/plugins/unleash/util/predicates/IsSnapshotDependencyTest.java
@@ -1,30 +1,51 @@
-// package com.itemis.maven.plugins.unleash.util.predicates;
-//
-// import org.apache.maven.model.Dependency;
-// import org.junit.runner.RunWith;
-//
-// import com.tngtech.java.junit.dataprovider.DataProvider;
-// import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-//
-// @RunWith(DataProviderRunner.class)
-// public class IsSnapshotDependencyTest {
-// @DataProvider
-// public static Object[][] dependencies() {
-// return new Object[][] { { createDependency("1-SNAPSHOT"), true }, { createDependency("1"), false },
-// { createDependency("1-snapshot"), true }, { createDependency("1-Alpha-SNAPSHOT"), true },
-// { createDependency("1.alpha"), false }, { createDependency(null), false } };
-// }
-//
-// // FIXME: adapt the test to the new feature with property resolving!
-// // @Test
-// // @UseDataProvider("dependencies")
-// // public void TestApply(Dependency d, boolean expected) {
-// // Assert.assertEquals(expected, IsSnapshotDependency.INSTANCE.apply(d));
-// // }
-//
-// private static Dependency createDependency(String version) {
-// Dependency d = new Dependency();
-// d.setVersion(version);
-// return d;
-// }
-// }
+package com.itemis.maven.plugins.unleash.util.predicates;
+
+import org.apache.maven.model.Dependency;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import com.itemis.maven.plugins.unleash.util.PomPropertyResolver;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+@RunWith(DataProviderRunner.class)
+public class IsSnapshotDependencyTest {
+  @DataProvider
+  public static Object[][] dependencies() {
+    return new Object[][] { { createDependencyAndInitPropertyResolver("1-SNAPSHOT", "1-SNAPSHOT"), true },
+        { createDependencyAndInitPropertyResolver("1", "1"), false },
+        { createDependencyAndInitPropertyResolver("1-snapshot", "1-snapshot"), true },
+        { createDependencyAndInitPropertyResolver("1-Alpha-SNAPSHOT", "1-Alpha-SNAPSHOT"), true },
+        { createDependencyAndInitPropertyResolver("1.alpha", "1.alpha"), false },
+        { createDependencyAndInitPropertyResolver("${version.test1}", "${version.test1}"), false },
+        { createDependencyAndInitPropertyResolver("${version.test2}", "1-SNAPSHOT"), true },
+        { createDependencyAndInitPropertyResolver(null, null), false } };
+  }
+
+  private static PomPropertyResolver propertyResolver;
+  private static IsSnapshotDependency predicate;
+
+  @BeforeClass
+  public static void init() {
+    propertyResolver = Mockito.mock(PomPropertyResolver.class);
+    predicate = new IsSnapshotDependency(propertyResolver);
+  }
+
+  @Test
+  @UseDataProvider("dependencies")
+  public void TestApply(Dependency p, boolean expected) {
+    Assert.assertEquals(expected, predicate.apply(p));
+  }
+
+  private static Dependency createDependencyAndInitPropertyResolver(String version, String resolverOutput) {
+    Mockito.when(propertyResolver.expandPropertyReferences(version)).thenReturn(resolverOutput);
+
+    Dependency d = new Dependency();
+    d.setVersion(version);
+    return d;
+  }
+}
--- plugin/src/test/java/com/itemis/maven/plugins/unleash/util/predicates/IsSnapshotPluginTest.java
+++ plugin/src/test/java/com/itemis/maven/plugins/unleash/util/predicates/IsSnapshotPluginTest.java
@@ -1,30 +1,51 @@
-// package com.itemis.maven.plugins.unleash.util.predicates;
-//
-// import org.apache.maven.model.Plugin;
-// import org.junit.runner.RunWith;
-//
-// import com.tngtech.java.junit.dataprovider.DataProvider;
-// import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-//
-// @RunWith(DataProviderRunner.class)
-// public class IsSnapshotPluginTest {
-// @DataProvider
-// public static Object[][] plugins() {
-// return new Object[][] { { createPlugin("1-SNAPSHOT"), true }, { createPlugin("1"), false },
-// { createPlugin("1-snapshot"), true }, { createPlugin("1-Alpha-SNAPSHOT"), true },
-// { createPlugin("1.alpha"), false }, { createPlugin(null), false } };
-// }
-//
-// // FIXME: adapt the test to the new feature with property resolving!
-// // @Test
-// // @UseDataProvider("plugins")
-// // public void TestApply(Plugin p, boolean expected) {
-// // Assert.assertEquals(expected, IsSnapshotPlugin.INSTANCE.apply(p));
-// // }
-//
-// private static Plugin createPlugin(String version) {
-// Plugin p = new Plugin();
-// p.setVersion(version);
-// return p;
-// }
-// }
+package com.itemis.maven.plugins.unleash.util.predicates;
+
+import org.apache.maven.model.Plugin;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import com.itemis.maven.plugins.unleash.util.PomPropertyResolver;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+@RunWith(DataProviderRunner.class)
+public class IsSnapshotPluginTest {
+  @DataProvider
+  public static Object[][] plugins() {
+    return new Object[][] { { createPluginAndInitPropertyResolver("1-SNAPSHOT", "1-SNAPSHOT"), true },
+        { createPluginAndInitPropertyResolver("1", "1"), false },
+        { createPluginAndInitPropertyResolver("1-snapshot", "1-snapshot"), true },
+        { createPluginAndInitPropertyResolver("1-Alpha-SNAPSHOT", "1-Alpha-SNAPSHOT"), true },
+        { createPluginAndInitPropertyResolver("1.alpha", "1.alpha"), false },
+        { createPluginAndInitPropertyResolver("${version.test1}", "${version.test1}"), false },
+        { createPluginAndInitPropertyResolver("${version.test2}", "1-SNAPSHOT"), true },
+        { createPluginAndInitPropertyResolver(null, null), false } };
+  }
+
+  private static PomPropertyResolver propertyResolver;
+  private static IsSnapshotPlugin predicate;
+
+  @BeforeClass
+  public static void init() {
+    propertyResolver = Mockito.mock(PomPropertyResolver.class);
+    predicate = new IsSnapshotPlugin(propertyResolver);
+  }
+
+  @Test
+  @UseDataProvider("plugins")
+  public void TestApply(Plugin p, boolean expected) {
+    Assert.assertEquals(expected, predicate.apply(p));
+  }
+
+  private static Plugin createPluginAndInitPropertyResolver(String version, String resolverOutput) {
+    Mockito.when(propertyResolver.expandPropertyReferences(version)).thenReturn(resolverOutput);
+
+    Plugin p = new Plugin();
+    p.setVersion(version);
+    return p;
+  }
+}
